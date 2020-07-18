8c1da19ae432d897e21e0a164279e723fbdc7a3d
Michael Riffle <mriffle@uw.edu>
2020-07-18 08:06:27 -0700

    Change matching pepxml to those ending in ".target.pep.xml"

--- src/main/java/org/yeastrc/limelight/xml/crux_comet_percolator/constants/CruxConstants.java
+++ src/main/java/org/yeastrc/limelight/xml/crux_comet_percolator/constants/CruxConstants.java
@@ -2,7 +2,7 @@ package org.yeastrc.limelight.xml.crux_comet_percolator.constants;
 
 public class CruxConstants {
 
-    public static final String pepXMLSuffix = ".pep.xml";
+    public static final String pepXMLSuffix = ".target.pep.xml";
     public static final String cruxOutputPercolatorXMLFileName = "percolator.pout.xml";
     public static final String percolatorLogFile = "percolator.log.txt";
     public static final String cometLogFile = "comet.log.txt";
