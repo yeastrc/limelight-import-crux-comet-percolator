f1c5833fc5d5ec17acf8d1590cbf0042ae99680d
Michael Riffle <mriffle@uw.edu>
2020-10-13 12:34:14 -0700

    Fix bug parsing variable mods, round to 4 decimal places instead of two.

--- src/main/java/org/yeastrc/limelight/xml/crux_comet_percolator/utils/ReportedPeptideUtils.java
+++ src/main/java/org/yeastrc/limelight/xml/crux_comet_percolator/utils/ReportedPeptideUtils.java
@@ -38,7 +38,7 @@ public class ReportedPeptideUtils {
 		    
 		    if( mods.containsKey( position ) ) {
 		    	
-		    	BigDecimal v = mods.get( position ).setScale( 2, RoundingMode.HALF_UP );
+		    	BigDecimal v = mods.get( position ).setScale( 4, RoundingMode.HALF_UP );
 		    	sb.append( "[" );
 		    	sb.append( v.toString() );
 		    	sb.append( "]" );
@@ -48,7 +48,7 @@ public class ReportedPeptideUtils {
 
 		// add in n-term mod
 		if( mods.containsKey( 0 ) ) {
-			BigDecimal v = mods.get( 0 ).setScale( 2, RoundingMode.HALF_UP );
+			BigDecimal v = mods.get( 0 ).setScale( 4, RoundingMode.HALF_UP );
 
 			sb.insert( 0, "n[" + v.toString() + "]" );
 		}
@@ -56,7 +56,7 @@ public class ReportedPeptideUtils {
 
 		// add in c-term mod
 		if( mods.containsKey( sequence.length() + 1 ) ) {
-			BigDecimal v = mods.get( sequence.length() + 1 ).setScale( 2, RoundingMode.HALF_UP );
+			BigDecimal v = mods.get( sequence.length() + 1 ).setScale( 4, RoundingMode.HALF_UP );
 
 			sb.append( "c[" + v.toString() + "]" );
 		}
